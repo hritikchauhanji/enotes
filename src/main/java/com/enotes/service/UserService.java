dcda278426607b85d6a0ed4c6a8a6295fbf2fc6a
hritikchauhanji <hritikchji@gmail.com>
2025-02-11 00:07:26 +0530

    send email api

--- src/main/java/com/enotes/service/UserService.java
+++ src/main/java/com/enotes/service/UserService.java
@@ -2,7 +2,11 @@ package com.enotes.service;
 
 import com.enotes.dto.PasswordChangeRequest;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public interface UserService{
  
 	void passwordChange(PasswordChangeRequest passwordChangeRequest);
+
+	void sendEmailPasswordReset(String email, HttpServletRequest request) throws Exception;
 }
--- src/main/java/com/enotes/service/impl/UserServiceImpl.java
+++ src/main/java/com/enotes/service/impl/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package com.enotes.service.impl;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import com.enotes.dto.PasswordChangeRequest;
 import com.enotes.entity.User;
+import com.enotes.exceptionhandling.ResourceNotFoundException;
 import com.enotes.repository.UserRepository;
 import com.enotes.service.UserService;
 import com.enotes.util.CommonUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -29,6 +35,20 @@ public class UserServiceImpl implements UserService {
 		String newPassword = encoder.encode(passwordChangeRequest.getNewPassword());
 		loggedInUser.setPassword(newPassword);
 		userRepository.save(loggedInUser);
+	}
+
+	@Override
+	public void sendEmailPasswordReset(String email, HttpServletRequest request) throws Exception {
+		User user = userRepository.findByEmail(email);
+		if(ObjectUtils.isEmpty(user)) {
+			throw new ResourceNotFoundException("Invalid Email...");
+		}
+		String resetToken = UUID.randomUUID().toString();
+		user.getStatus().setPasswordResetToken(resetToken);
+		User updateUser = userRepository.save(user);
+		
+		String url = CommonUtil.getUrl(request);
+		
 	}
 
 }
